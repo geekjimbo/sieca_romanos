e198e75bd480b4096dc4fcf001efdd7e9613882b
Jimmy Figueroa <jimmyfigueroa@me.com>
2016-08-16 23:28:27 -0600

    fixed snipets

--- app/helpers/romans_snipet.rb
+++ app/helpers/romans_snipet.rb
@@ -1,16 +1,16 @@
-def convert(in_arabic)
-  return "" if in_arabic == 0
-  arabic, roman = convert_factors(in_arabic)
-  roman + convert(in_arabic - arabic)
-end
-
-ARABIC_FACTORS = [
-                  [9, "IX"],
-                  [5, "V"],
-                  [4, "IV"],
-                  [1,"I"]
-                 ]
-
-def convert_factors(in_arabic)
-  ARABIC_FACTORS.find {|arabic, _| arabic <= in_arabic}
-end
+#def convert(in_arabic)
+#  return "" if in_arabic == 0
+#  arabic, roman = convert_factors(in_arabic)
+#  roman + convert(in_arabic - arabic)
+#end
+#
+#ARABIC_FACTORS = [
+#                  [9, "IX"],
+#                  [5, "V"],
+#                  [4, "IV"],
+#                  [1,"I"]
+#                 ]
+#
+#def convert_factors(in_arabic)
+#  ARABIC_FACTORS.find {|arabic, _| arabic <= in_arabic}
+#end
